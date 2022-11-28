8d8c5e702eaaee959fcf66528a1482bb501de75d
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2022-11-28 18:17:27 -0300

    entrega

---
+++ entrega.js
@@ -0,0 +1,275 @@
+// ej 1 comentar codigo
+
+// asd
+/* 
+asd
+*/
+
+// ej 2
+// running tests
+// You should declare myName with the var keyword, ending with a semicolon
+// tests completed
+
+var myName;
+
+
+
+// ej 3
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+// ej 4
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+// ej 5
+var a = 9;
+
+// ej 6
+var myFirstName = "gonzi";
+var myLastName = "flow";
+
+// ej 7
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// ej 8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// ej 9
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// ej 10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+// ej 11
+
+const sum = 10 + 10;
+
+// ej 12
+
+const difference = 45 - 33;
+
+// ej 13
+
+const product = 8 * 10;
+
+// ej 14
+
+const quotient = 66 / 33;
+
+// ej 15
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+// ej 16
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+// ej 17
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+let myDecimal = 5.7;
+
+// ej 18
+
+const product = 2.0 * 2.5;
+
+// ej 19
+
+const quotient = 4.4 / 2.0; // Change this line
+
+// ej 20
+
+const remainder = 11 % 3;
+
+// ej 21
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// ej 22
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=  1;
+
+// ej 23
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+// ej 24
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=  12;
+b /=  4;
+c /=  11;
+
+// ej 25
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// ej 26
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// ej 27
+
+const myStr = "This is the start. "+"This is the end."; // Change this line
+
+// ej 28
+
+let myStr = "This is the first sentence. ";
+myStr+= "This is the second sentence.";
+
+// ej 29
+
+// Only change code below this line
+const myName = "Gonziflow";
+const myStr = "My name is " + myName + "and I am well!";
+
+// ej 30
+
+
+// Change code below this line
+const someAdjective = "cool";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective
+
+// ej 31
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// ej 32
+
+
+// Setup
+let firstLetterOfLastName = "Jorge";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// ej 33
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// ej 34
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// ej 35
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+
+// ej 36
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// ej 37
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+// ej 38
+
+// Only change code below this line
+const myArray = ["myArray", 2];
+
+// ej 39
+
+// Only change code below this line
+const myArray = [["myArray", 3], ["myOtroArray", 69]];
+
+// ej 40
+
+const myArray = [50, 60, 70];
+
+let myData = myArray[0]
